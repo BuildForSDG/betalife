db14102dc9f5a59b7caf8fcf8efe286df6c8851b
Ochuko Ekrresa <kelvinekrresa@gmail.com>
2020-05-25 23:53:49 +0100

    build: update seeders

--- src/database/seeders/20200522193111-demo-user.js
+++ src/database/seeders/20200522193111-demo-user.js
@@ -1,15 +1,21 @@
+const { image } = require('faker');
+
 module.exports = {
   up: queryInterface => {
     return queryInterface.bulkInsert(
       'users',
       [
         {
+          id: '1bb47d48-a93e-4f4d-ac01-6c2de3f3249d',
           first_name: 'John',
           last_name: 'Doe',
           email: 'example@example.com',
-          phone_no: '00123456789',
-          photo: 'JohnDoe.png',
+          password: '$2y$10$25ubzgnL6xCruAkNDmk5Vu/HDeB.DUTO8m21I50R5wQ.c5edWsutu',
+          phone: '00123456789',
+          photo: image.avatar(),
           address: 'xyz street, abc city',
+          city: 'Abc',
+          state: 'Lagos',
           enabled: 1,
           createdAt: new Date(),
           updatedAt: new Date()
--- src/database/seeders/20200522193710-demo-super-admin.js
+++ src/database/seeders/20200522193710-demo-super-admin.js
@@ -4,7 +4,8 @@ module.exports = {
       'super_admins',
       [
         {
-          userId: 2,
+          id: 'bbb1ef27-f6fc-41a5-9962-c8d545559119',
+          user_id: '1bb47d48-a93e-4f4d-ac01-6c2de3f3249d',
           createdAt: new Date(),
           updatedAt: new Date()
         }
